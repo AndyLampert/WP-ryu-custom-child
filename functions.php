ce695488cae1c12cb153a7f7e36f4fb423471ed5
AndyLampert <lamperta@gmail.com>
2014-04-12 11:14:52 -0600

    added custom nav to ryu_custom.js

---
+++ functions.php
@@ -0,0 +1,11 @@
+<!-- will not override the functions.php in the parent theme -->
+
+<?php 
+  function ryuCustomEnqueue() {
+    // This will enqueue a script file called ryu_custom.js
+    wp_enqueue_script("ryu_custom", get_stylesheet_directory_uri()."/ryu_custom.js", array("jquery"));
+    // This will enqueue a style file called font-awesome.min.css
+    wp_enqueue_style("font_awesome", get_stylesheet_directory_uri()."/font-awesome.min.css", array("style.css"));
+  }
+  add_action("wp_enqueue_scripts", "ryuCustomEnqueue");
+ ?>
---
+++ ryu_custom.js
@@ -0,0 +1,34 @@
+// Set the $ to = jQuery
+(function($){
+  $(document).ready(function(){
+    var navOffset = $('#site-navigation').position().top;
+    $(document).scroll(function(event){ 
+      var scrollTop = $(document).scrollTop();
+      var navHeight = $('#site-navigation').outerHeight();
+      // console.log('scroll: ', scrollTop);
+      // console.log('nav: ', navOffset);
+      // console.log('navheight:', navHeight);
+      // console.log(scrollTop + navOffset + navHeight);
+
+      // When the user scrolls to 357, apply CSS to stick the nav to the top
+      // currently set to a specific value. 
+      if(scrollTop >= 357) {
+        $('#site-navigation').css({
+          "position": "fixed",
+          "top": "0",
+          "width": "100%"
+        });
+          // adds padding-top to the primary content id container to offset the nav height (to cancel jumpy movement)
+          // navHeight defaults to a number, add "px" to make it a readable value
+          $('#primary').css("padding-top", navHeight + "px");
+        }
+        else {
+          // when you scroll back above, put it back in place
+          $('#site-navigation').css("position", "static");
+          // AND 
+          $('#primary').css("padding-top", 0);
+        }
+      });    
+  });
+})(jQuery);
+
